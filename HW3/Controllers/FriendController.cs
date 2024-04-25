b19c70ee1a8e980395efd2a171d45dea4ca5dd93
denis_bezdolny <d.i.d.c.o.d.a@gmail.com>
2024-04-25 14:07:53 +0300

    copying privious project

---
+++ HW3/Controllers/FriendController.cs
@@ -0,0 +1,84 @@
+﻿using HW2.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HW2.Controllers
+{
+    public class FriendController : Controller
+    {
+       
+        static List<Friend> friendList = new List<Friend>()
+        {
+            new Friend{ FriendId = 1,  FriendName = "Ivan",  Place = "Minsk" },
+            new Friend{ FriendId = 2,  FriendName = "Sergey", Place = "Grodno" },
+            new Friend{ FriendId = 3,  FriendName = "Maria",  Place = "Gomel" }
+        };
+
+
+        public ActionResult Index()
+        {
+
+            return View(friendList);
+
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Friend friend)
+        {
+            if (ModelState.IsValid)
+            {
+
+                friend.FriendId = friendList.Count+1;
+                friendList.Add(friend);
+
+                return RedirectToAction(nameof(Index));
+            }
+            return View(friend);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            var friend = friendList[id.Value-1];
+            return View(friend);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Friend friend)
+        {
+            if (ModelState.IsValid)
+            {
+                
+                friendList[friend.FriendId - 1].FriendName = friend.FriendName;
+                friendList[friend.FriendId - 1].Place = friend.Place;
+
+                return RedirectToAction("Index");
+            }
+            return View(friend);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            var friend = friendList[id.Value - 1];
+            return View(friend);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(Friend friend)
+        {
+            if (ModelState.IsValid)
+            {
+                friendList.RemoveAt(friend.FriendId - 1);
+                return RedirectToAction("Index");
+            }
+            return View(friend);
+
+        }
+    }
+}
---
+++ HW3/Models/Friend.cs
@@ -0,0 +1,22 @@
+﻿namespace HW2.Models
+{
+    public class Friend
+    {
+        public int FriendId { get; set; }
+        public string FriendName { get; set; }
+        public string Place { get; set; }
+
+
+        //public static List<Friend> GetFriendsList()
+        //{
+        //    List<Friend> result = new List<Friend>()
+        //    {
+        //        new Friend{ FriendId = 1,  FriendName = "Ivan",  Place = "Minsk" },
+        //        new Friend{ FriendId = 2,  FriendName = "Sergey", Place = "Grodno" },
+        //        new Friend{ FriendId = 3,  FriendName = "Maria",  Place = "Gomel" }
+        //    };
+
+        //    return result;
+        //}
+    }
+}
