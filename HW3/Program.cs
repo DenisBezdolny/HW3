af8ee05e983ad2a3308783e3f803109f0aec0511
denis_bezdolny <d.i.d.c.o.d.a@gmail.com>
2024-04-25 22:28:34 +0300

    Add new service StubFriendService, is realising other variant of IFriendServise

--- HW3/Program.cs
+++ HW3/Program.cs
@@ -1,14 +1,31 @@
 using HW3.Models.Abstract_entities;
 using HW3.Models.Entities;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddTransient<IFriendService, Friend>();
-//builder.Services.AddTransient<IFriendService, StubFriendService>();
+
+
+bool isDevelopment = builder.Environment.IsDevelopment();
+
+if (isDevelopment)
+{
+    builder.Services.AddTransient<IFriendService, StubFriendService>();
+}
+else
+{
+    builder.Services.AddTransient<IFriendService, StubFriendService>();
+}
+
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -16,6 +33,7 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    
 }
 
 app.UseHttpsRedirection();
