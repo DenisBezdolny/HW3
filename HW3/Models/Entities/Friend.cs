72d146822a9206a042e35453487a92b6efb5680c
denis_bezdolny <d.i.d.c.o.d.a@gmail.com>
2024-04-25 15:08:10 +0300

    Add new interface IFriendService, + realization it's method in Friend. Replace Friend in new fold "Entities"

---
+++ HW3/Models/Entities/Friend.cs
@@ -0,0 +1,16 @@
+﻿using HW3.Models.Abstract_entities;
+
+namespace HW3.Models.Entities
+{
+    public class Friend:IFriendService
+    {
+        public int FriendId { get; set; }
+        public string FriendName { get; set; }
+        public string Place { get; set; }
+
+        public List<Friend> GetFriends()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
